c20c8b297aec953f1fdf41c80684cc00c113dc68
ponytail <wp623945740@hotmail.com>
2017-08-07 08:45:59 -0400

    nmap parse

---
+++ utils/parse/nmap.js
@@ -0,0 +1,84 @@
+var cheerio = require('cheerio');
+var fs = require('fs');
+var util = require('util')
+var xml2sj = require('xml2js');
+
+function getXMLContent(file){
+    return new Promise(function(resolve, reject){
+        var encoding = 'utf-8';
+        fs.readFile(file, function(err, data){
+            if(err){
+                reject(err)
+            }
+            else{
+                resolve(data.toString('utf-8'));
+            }           
+        })
+    })
+}
+
+getXMLContent('')
+.then(function(xml){
+    return new Promise(function(resolve, reject){
+        var parse = new xml2sj.Parser()
+        parse.parseString(xml, function(err, object){
+            if(err){
+                reject(err)
+            }
+            else{
+                resolve(object);
+            }
+        })
+    })
+})
+.then(function(object){
+    object.nmaprun.host.forEach(function(host){
+        /*
+        host.address.forEach(function(addr){
+            console.log(addr.$.addr)
+        })
+        */
+        host.ports.forEach(function(result){
+            result.port.forEach(function(port){
+                console.log(port.$.portid)
+            })
+        })
+    })
+})
+/*
+getXMLContent(process.argv[2])
+.then(function(xml){
+    const $ = cheerio.load(xml, {
+        "normalizeWhitespace" : true,
+        "xmlMode" : true
+    });
+
+    $('service').each(function(index, service){
+        var port = service.parent;
+
+        var rmi_registry = ''
+        var curr = service;
+        while(curr){
+            if(curr.name === 'script'){
+                rmi_registry = curr.attribs['output'];
+            }
+            curr = curr.next;
+        }
+
+        var address = ''
+        curr = port.parent;
+        while(curr){
+            if(curr.name === 'address'){
+                address = util.format('%s:%s', curr.attribs['addr'], port.attribs['portid'])
+            }
+            curr = curr.prev;
+        }
+
+        console.log(address, rmi_registry)
+    })
+
+})
+.catch(function(err){
+    console.log(err)
+})
+*/
