82fea6183e16af63320ca1455fb04dabea105fa1
ponytail <wp623945740@hotmail.com>
2017-08-22 13:54:51 -0400

    whois ripe

--- libs/whois/parse/index.js
+++ libs/whois/parse/index.js
@@ -1,5 +1,34 @@
 module.exports = (function(){
     //var logger = options.logger;
+    function apnic(data){
+        var infos = [];
+            while(true){
+                var m = data.lastIndexOf('% Information related to');
+                if(m === -1){
+                    break;
+                }
+                infos.push(data.substring(m, data.length));
+                data = data.substring(0, m);
+            }
+            infos.reverse();
+            /*
+                inetnum:        218.85.0.0 - 218.86.127.255
+                netname:        CHINANET-FJ
+            */
+            return infos.map(function(i){
+                var ret = {
+                    'detail' : i
+                }
+                var netname = i.match(/netname:\s*([^\n]*)/)
+                var inetnum = i.match(/inetnum:\s*([^\n]*)/)
+                if(netname !== null && inetnum !== null){
+                    //logger.info('%s\t%s', netname[1], inetnum[1])
+                    ret['netname'] = netname[1];
+                    ret['netblock'] = inetnum[1];
+                }
+                return ret;
+            })
+    }
     return {
         'whois.arin.net' : function(data){
             var infos = data.match(/#\s*start([\w\W]*?)#\s*end/g);
@@ -28,34 +57,7 @@ module.exports = (function(){
             }
         },
 
-        'whois.apnic.net' : function(data){
-            var infos = [];
-            while(true){
-                var m = data.lastIndexOf('% Information related to');
-                if(m === -1){
-                    break;
-                }
-                infos.push(data.substring(m, data.length));
-                data = data.substring(0, m);
-            }
-            infos.reverse();
-            /*
-                inetnum:        218.85.0.0 - 218.86.127.255
-                netname:        CHINANET-FJ
-            */
-            return infos.map(function(i){
-                var ret = {
-                    'detail' : i
-                }
-                var netname = i.match(/netname:\s*([^\n]*)/)
-                var inetnum = i.match(/inetnum:\s*([^\n]*)/)
-                if(netname !== null && inetnum !== null){
-                    //logger.info('%s\t%s', netname[1], inetnum[1])
-                    ret['netname'] = netname[1];
-                    ret['netblock'] = inetnum[1];
-                }
-                return ret;
-            })
-        }
+        'whois.apnic.net' : apnic,
+        'whois.ripe.net' : apnic
     }
 })()
